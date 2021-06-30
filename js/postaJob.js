0b94c512c21cd54f87aa0c3aaeeb3ed4db149219
Odeh <abuzaid163@hotmail.com>
2021-06-30 09:48:18 +0300

    joppostJS

--- js/postaJob.js
+++ js/postaJob.js
@@ -1,5 +1,6 @@
 'user strict';
-
+let formsubmit = document.getElementById( 'submit' );
+formsubmit.addEventListener( 'click', newJobposts );
 let pid,jobTitle, jobDescription, jobCategory,jobShift, payRate,companySrc;
 let id,imgSrc, website,phone,size,industry;
 pid = `${generateNewID( posts.newPosts ) } `;
@@ -9,36 +10,24 @@ jobCategory = document.getElementById( 'jc' ).options[document.getElementById( '
 jobShift = document.getElementById( 'Jobshift' ).options[document.getElementById( 'Jobshift' ).selectedIndex].value;
 payRate = document.getElementById( 'payR' ).value;
 companySrc = 'default.jpg';
-
-
 id = `${generateNewID( company.companies )}`;
 imgSrc = 'default.jpg';
 website = document.getElementById( 'cWebsite' ).value ;
 phone = document.getElementById( 'cPhone' ).value ;
 size = document.getElementById( 'cSize' ).options[document.getElementById( 'cSize' ).selectedIndex].value;
 industry = document.getElementById( 'cIndustry' ).options[document.getElementById( 'cIndustry' ).selectedIndex].value;
-
-let formsubmit = document.getElementById( 'submit' );
-formsubmit.addEventListener( 'click', newJobposts );
-function newJobposts ( ){
+function newJobposts (){
   posts.addPost ( pid, id, jobTitle, jobDescription, jobShift, jobCategory, payRate, companySrc );
   posts.saveToLocalStorage();
-
-
   company.addCompany( id, industry, imgSrc, website, phone, size );
-
   company.saveToLocalStorage();
-
   // location.href = '../index.html';
 }
 function generateNewID( array ) {
   return parseInt( array[array.length - 1].id ) + 1;
 }
-
-
 function loadfromLocalStorage(){
   let comaniesdata =  JSON.parse( localStorage.getItem( 'companies' ) ) || [];
-
   if( comaniesdata ){
     company.companies = [];
     for ( let i = 0 ; i < comaniesdata.length ;i++ ) {
@@ -51,10 +40,7 @@ function loadfromLocalStorage(){
         comaniesdata[i].size );
     }
   }
-
-
   let postsArr =  JSON.parse( localStorage.getItem( 'posts' ) ) || [];
-
   if( postsArr ){
 
     posts.newPosts = [];
@@ -68,11 +54,8 @@ function loadfromLocalStorage(){
         postsArr[i].payRate,
         postsArr[i].companySrc );
     }
-
   }
 
 
-
 }
-
 loadfromLocalStorage();
